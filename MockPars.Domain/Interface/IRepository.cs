d3ac6f529ad86690d6de21d038c27914d131abd6
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-13 19:41:49 +0330

    add IRepository

---
+++ MockPars.Domain/Interface/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace MockPars.Domain.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<IQueryable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+        public IQueryable<T> GetAllWithIncludes(
+            Func<IQueryable<T>, IQueryable<T>> includeFunc = null,
+            Expression<Func<T, bool>> filter = null);
+    }
+}
