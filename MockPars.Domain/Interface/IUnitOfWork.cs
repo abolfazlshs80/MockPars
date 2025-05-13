b9240984a96776cb07134b0885fe18525e0f398a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-13 19:41:58 +0330

    add IUnitOfWork

---
+++ MockPars.Domain/Interface/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace MockPars.Domain.Interface
+{
+    public interface IUnitOfWork : IDisposable
+    {
+
+    
+        IRepository<T> Repository<T>() where T : class;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
