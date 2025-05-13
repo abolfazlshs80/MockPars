77e1dc840c299ab561c5536abc9c2e164dac1c64
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-13 19:42:32 +0330

    add Tables

---
+++ MockPars.Domain/Models/Tables.cs
@@ -0,0 +1,18 @@
+﻿namespace MockPars.Domain.Models;
+
+public class Tables
+{
+    public int Id { get; set; }
+    public string TableName { get; set; }
+
+    public string Slug { get; set; }
+    public bool IsGetAll { get; set; }
+    public bool IsGet { get; set; }
+    public bool IsPut { get; set; }
+    public bool IsPost { get; set; }
+    public bool IsDelete { get; set; }
+    public int DatabaseId { get; set; }
+    public Databases Databases { get; set; }
+
+    public HashSet<Columns> Columns { get; set; }
+}
