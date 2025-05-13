591dba7704deb0afba0b6274246ac871795a5d7f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-13 19:42:23 +0330

    add Databases

---
+++ MockPars.Domain/Models/Databases.cs
@@ -0,0 +1,12 @@
+﻿namespace MockPars.Domain.Models;
+
+public class Databases
+{
+    public int Id { get; set; }
+    public string DatabaseName { get; set; }
+
+    public string Slug { get; set; }
+    public string UserId { get; set; }
+    public User User { get; set; }
+    public HashSet<Tables> Tables { get; set; }
+}
