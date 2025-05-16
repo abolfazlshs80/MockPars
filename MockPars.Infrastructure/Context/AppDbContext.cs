fc4d8e5847895e2225e8bf91c04a32d46c389613
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-16 17:42:16 +0330

    add appdbcontext

---
+++ MockPars.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using MockPars.Domain.Models;
+
+namespace MockPars.Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+          //  modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetAssembly(typeof(AppDbContext)));
+          
+        }
+
+        #region DbSet
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Databases> Databases { get; set; }
+        public DbSet<Columns> Columns { get; set; }
+        public DbSet<Tables> Tables { get; set; }
+
+
+
+
+        #endregion
+
+
+    }
+}
