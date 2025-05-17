17f2e56f55e07e04e3a7977b9b230b5020779069
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-17 17:51:50 +0330

    update by option pattern

--- MockPars.WebApi/Program.cs
+++ MockPars.WebApi/Program.cs
@@ -1,5 +1,8 @@
 
 using MockPars.Infrastructure;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MockPars.WebApi
 {
@@ -14,6 +17,27 @@ namespace MockPars.WebApi
             
             builder.Services.AddControllers();
             builder.Services.RegisterInfrastructureServices(builder.Configuration);
+
+            #region Config JWT
+            var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            #endregion
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
