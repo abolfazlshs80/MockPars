96717a5169452919cf5d5737398f058d3822e4c5
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-18 21:38:25 +0330

    add extention

---
+++ MockPars.Application/Extention/UserExtentions.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace MockPars.Application.Extention
+{
+    public static class UserExtentions
+    {
+        public static string GetUserId(this IPrincipal principal)
+        {
+            var user = (ClaimsPrincipal)principal;
+
+            return user.GetUserId();
+        }
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            if (claimsPrincipal != null)
+            {
+                var data = claimsPrincipal.Claims.SingleOrDefault(s => s.Type == ClaimTypes.NameIdentifier);
+                if (data != null) return data.Value;
+
+            }
+
+            return "";
+        }
+    }
+}
