7800534c7e6180d9ee06256a2577b326b173fb57
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-19 17:51:33 +0330

    add registerdto

---
+++ MockPars.Application/DTO/Users/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockPars.Application.DTO.Users
+{
+    public class RegisterDto
+    {
+        [RegularExpression("^[A-Za-z0-9]+$",ErrorMessage = "نام کاربری باید انگلیسی باشد")]
+        public string UserName { get; set; }
+        [MinLength(4,ErrorMessage = "حداقل 4 حرف باشد")]
+        public string Password { get; set; }
+    }
+}
