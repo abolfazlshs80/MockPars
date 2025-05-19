8e4ba70a3b48acdb055b14a615570ae7cc3d9960
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-19 17:53:17 +0330

    add user controller and register method

---
+++ MockPars.WebApi/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using MockPars.Application.DTO.Users;
+using MockPars.Application.Services.Interfaces;
+
+namespace MockPars.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController(IUserService userService) : ControllerBase
+    {
+        [HttpPost("RegisterUser")]
+        public async Task<IActionResult> GetAll(RegisterDto model, CancellationToken ct)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(model);
+
+            var result = await userService.Register(model, ct);
+            if (result.IsError)
+                return BadRequest(model);
+
+            return Ok(result.Value);
+        }
+
+    }
+}
