0de0f94ac219f1419d90ff1be93e87f3d88dd847
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-20 15:54:07 +0330

    add message static

---
+++ MockPars.Application/Static/Message/DatabaseMessageStatic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockPars.Application.Static.Message
+{
+    public static class DatabaseMessage
+    {
+        public const string NotFound = "پیدا نشد";
+    }
+}
