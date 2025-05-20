41a165b7409c2870f1292617c10df67a8cab7e12
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-20 15:53:57 +0330

    crud for service

--- MockPars.Application/Services/Implementation/DatabaseService.cs
+++ MockPars.Application/Services/Implementation/DatabaseService.cs
@@ -1,7 +1,83 @@
-﻿using MockPars.Application.Services.Interfaces;
+﻿using ErrorOr;
+using MockPars.Application.DTO.Database;
+using MockPars.Application.DTO.Users;
+using MockPars.Application.Generator;
+using MockPars.Application.Services.Interfaces;
+using MockPars.Application.Static.Message;
+using MockPars.Domain.Interface;
+using MockPars.Domain.Models;
+using System.Reflection;
 
 namespace MockPars.Application.Services.Implementation;
 
-public class DatabaseService : IDatabaseService
+public class DatabaseService(IUnitOfWork unitOfWork) : IDatabaseService
 {
+    public async Task<ErrorOr<int>> CreateDatabase(CreateDatabaseDto model, CancellationToken ct)
+    {
+        var existsUser = await unitOfWork.UserRepository.ExistsAsync(model.UserId, ct);
+        if (!existsUser)
+            return ErrorOr.Error.NotFound(description: UserMessageStatic.NotFound_User);
+
+        var database = new Databases()
+        {
+            Slug = model.Slug,
+            UserId = model.UserId,
+            DatabaseName = model.DatabaseName
+        };
+
+        await unitOfWork.DatabasesRepository.AddAsync(database, ct);
+        await unitOfWork.SaveChangesAsync(ct);
+        return database.Id;
+
+    }
+
+    public async Task<ErrorOr<int>> UpdateDatabase(UpdateDatabaseDto model, CancellationToken ct)
+    {
+        var existsUser = await unitOfWork.UserRepository.ExistsAsync(model.UserId, ct);
+        if (!existsUser)
+            return ErrorOr.Error.NotFound(description: UserMessageStatic.NotFound_User);
+
+        var findDatabase = await unitOfWork.DatabasesRepository.GetByIdAsync(model.Id, ct);
+        if (findDatabase is null)
+            return ErrorOr.Error.NotFound(description: DatabaseMessage.NotFound);
+
+        findDatabase.Slug = model.Slug;
+        findDatabase.UserId = model.UserId;
+        findDatabase.DatabaseName = model.DatabaseName;
+
+        await unitOfWork.DatabasesRepository.UpdateAsync(findDatabase, ct);
+        await unitOfWork.SaveChangesAsync(ct);
+        return findDatabase.Id;
+    }
+
+    public async Task<ErrorOr<bool>> DeleteDatabase(int Id, CancellationToken ct)
+    {
+        var findDatabase = await unitOfWork.DatabasesRepository.GetByIdAsync(Id, ct);
+        if (findDatabase is null)
+            return ErrorOr.Error.NotFound(description: DatabaseMessage.NotFound);
+        await unitOfWork.DatabasesRepository.DeleteAsync(findDatabase, ct);
+        await unitOfWork.SaveChangesAsync(ct);
+        return true;
+    }
+
+    public async Task<ErrorOr<DatabaseItemDto>> GetDatabaseById(int id,string userId, CancellationToken ct)
+    {
+        var findDatabase = await unitOfWork.DatabasesRepository.GetByIdAsync(id,userId, ct);
+        if (findDatabase is null)
+            return ErrorOr.Error.NotFound(description: DatabaseMessage.NotFound);
+
+        return new DatabaseItemDto(findDatabase.Id, findDatabase.DatabaseName, findDatabase.Slug);
+    }
+
+    public async Task<ErrorOr<IEnumerable<DatabaseItemDto>>> GetDatabasesByUserId(string userId, CancellationToken ct)
+    {
+        var findDatabase = await unitOfWork.DatabasesRepository.GetByUserIdAsync( userId, ct);
+        if (findDatabase is null)
+            return ErrorOr.Error.NotFound(description: DatabaseMessage.NotFound);
+
+
+        var a= findDatabase.Select(_ => new DatabaseItemDto(_.Id, _.DatabaseName, _.Slug)).ToList();
+      
+        return findDatabase.Select(_=> new DatabaseItemDto(_.Id, _.DatabaseName, _.Slug)).ToList();
+    }
 }
--- MockPars.Application/Services/Interfaces/IDatabaseService.cs
+++ MockPars.Application/Services/Interfaces/IDatabaseService.cs
@@ -1,6 +1,16 @@
-﻿namespace MockPars.Application.Services.Interfaces;
+﻿using ErrorOr;
+using MockPars.Application.DTO.Database;
+using MockPars.Application.DTO.Users;
+
+namespace MockPars.Application.Services.Interfaces;
 
 public interface IDatabaseService
 
 {
+
+    Task<ErrorOr<int>> CreateDatabase(CreateDatabaseDto model, CancellationToken ct);
+    Task<ErrorOr<int>> UpdateDatabase(UpdateDatabaseDto model, CancellationToken ct);
+    Task<ErrorOr<bool>> DeleteDatabase(int id, CancellationToken ct);
+    Task<ErrorOr<DatabaseItemDto>> GetDatabaseById(int id, string userId, CancellationToken ct);
+    Task<ErrorOr<IEnumerable<DatabaseItemDto>>> GetDatabasesByUserId(string userId, CancellationToken ct);
 }
