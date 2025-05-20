73526a9df59cd4b00d651a9e6e8ac61576df6123
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-20 15:58:12 +0330

    update user rep

--- MockPars.Domain/Interface/IRepository.cs
+++ MockPars.Domain/Interface/IRepository.cs
@@ -5,13 +5,13 @@ namespace MockPars.Domain.Interface
     public interface IRepository<T> where T : class
     {
        
-            Task<IEnumerable<T>> GetAllAsync();
-            Task<T> GetByIdAsync(int id);
+            Task<IEnumerable<T>> GetAllAsync(CancellationToken ct);
+            Task<T> GetByIdAsync(int id, CancellationToken ct);
             Task<T> FindAsync(Expression<Func<T, bool>> predicate);
-            Task<bool> ExistsAsync(int id);
-            Task<T> AddAsync(T entity);
-            Task UpdateAsync(T entity);
-            Task DeleteAsync(T entity);
+            Task<bool> ExistsAsync(int id, CancellationToken ct);
+            Task<T> AddAsync(T entity,CancellationToken ct);
+            Task UpdateAsync(T entity, CancellationToken ct);
+            Task DeleteAsync(T entity, CancellationToken ct);
         
         public IQueryable<T> GetAllWithIncludes(
             Func<IQueryable<T>, IQueryable<T>> includeFunc = null,
--- MockPars.Domain/Interface/IUserRepository.cs
+++ MockPars.Domain/Interface/IUserRepository.cs
@@ -6,4 +6,5 @@ namespace MockPars.Domain.Interface;
 public interface IUserRepository : IRepository<User>
 {
     Task<bool> Create(User user, CancellationToken ct);
+    Task<bool> ExistsAsync(string id, CancellationToken ct);
 }
