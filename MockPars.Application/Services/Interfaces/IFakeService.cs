2ec2d301b84dc4d564c92c10229f396c1005b5bb
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-25 16:00:15 +0330

    add fake service

---
+++ MockPars.Application/Services/Interfaces/IFakeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockPars.Application.Services.Interfaces
+{
+    public interface IFakeService
+    {
+        //Task
+    }
+}
