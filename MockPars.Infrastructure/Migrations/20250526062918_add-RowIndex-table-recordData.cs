39423d471525dabdd03c88a677948b106d6f4b51
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-26 16:28:43 +0330

    add migration addRowIndextablerecordData

---
+++ MockPars.Infrastructure/Migrations/20250526062918_add-RowIndex-table-recordData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MockPars.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRowIndextablerecordData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RowIndex",
+                table: "RecordData",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowIndex",
+                table: "RecordData");
+        }
+    }
+}
