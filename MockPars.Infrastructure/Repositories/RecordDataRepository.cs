a1109d98a6d31d8e6b3a1333dd41b93b20f5bff9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-26 16:33:55 +0330

    add GetLastRowByColumnIdAsync

--- MockPars.Infrastructure/Repositories/RecordDataRepository.cs
+++ MockPars.Infrastructure/Repositories/RecordDataRepository.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using MockPars.Domain.Interface;
 using MockPars.Domain.Models;
 using MockPars.Infrastructure.Context;
+using System.Text.RegularExpressions;
 
 namespace MockPars.Infrastructure.Repositories;
 
@@ -17,11 +19,20 @@ public class RecordDataRepository : Repository<RecordData>, IRecordDataRepositor
 
     public async Task<RecordData> GetByIdAsync(int id, int columnId, CancellationToken ct)
     {
-        return await  _context.RecordData.Where(a => a.ColumnsId.Equals(columnId) && a.Id.Equals(id)).FirstOrDefaultAsync(ct);
+        return await _context.RecordData.Where(a => a.ColumnsId.Equals(columnId) && a.Id.Equals(id)).FirstOrDefaultAsync(ct);
     }
 
     public async Task<IEnumerable<RecordData>> GetByColumnIdAsync(int columnId, CancellationToken ct)
     {
-        return await _context.RecordData.Where(a => a.ColumnsId.Equals(columnId) ).ToListAsync(ct);
+        return await _context.RecordData.Where(a => a.ColumnsId.Equals(columnId)).ToListAsync(ct);
+    }
+
+    public async Task<int> GetLastRowByColumnIdAsync(int columnId, CancellationToken ct)
+    {
+
+
+        var res = await _context.RecordData.Where(a => a.ColumnsId.Equals(columnId)).OrderByDescending(_ => _.RowIndex).FirstOrDefaultAsync(ct);
+        return res is null? 1 : res.RowIndex + 1;
     }
 }
+
