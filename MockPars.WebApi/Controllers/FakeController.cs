07a207e9e6707f25e45528cad59b43e0b5ae7ced
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-26 16:32:37 +0330

    add fake controller

---
+++ MockPars.WebApi/Controllers/FakeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using MockPars.Application.DTO.Column;
+using MockPars.Application.DTO.Users;
+using MockPars.Application.Services.Implementation;
+using MockPars.Application.Services.Interfaces;
+
+namespace MockPars.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FakeController(IFakeService fakeService) : ControllerBase
+    {
+        [HttpGet("{tableId}/{count}")]
+        public async Task<IActionResult> GenerateFakeData(int tableId, int count)
+        {
+            //if (!ModelState.IsValid)
+            //    return BadRequest(model);
+
+            var result = await fakeService.GenerateFakeData(tableId, count, HttpContext.RequestAborted);
+            if (result.IsError)
+                return BadRequest(string.Join(",", result.Errors.Select(a => a.Description)));
+
+            return Ok(result.Value);
+
+        }
+    }
+}
