3a3788fd362787af7fcde6d483486c445a4646d9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-26 16:31:46 +0330

    add GetAllRowDataAsync for column service

--- MockPars.Application/Services/Implementation/ColumnService.cs
+++ MockPars.Application/Services/Implementation/ColumnService.cs
@@ -1,4 +1,5 @@
 ﻿using ErrorOr;
+using MockPars.Application.DTO.@base;
 using MockPars.Application.DTO.Column;
 using MockPars.Application.Services.Interfaces;
 using MockPars.Application.Static.Message;
@@ -10,19 +11,40 @@ namespace MockPars.Application.Services.Implementation;
 
 public class ColumnService(IUnitOfWork unitOfWork) : IColumnService
 {
+    public async Task<List<Dictionary<string, string>>> GetAllRowDataAsync(int tableId, CancellationToken ct)
+    {
+        var columns = await unitOfWork.ColumnsRepository.GetAllRowDataAsync(tableId, ct);
+
+
+        // تمام رکوردها رو جمع کن و بر اساس RowIndex گروه‌بندی کن
+        var allRecords = columns.SelectMany(c => c.RecordData.Select(r => new
+        {
+            ColumnName = c.ColumnName,
+            r.Value,
+            r.RowIndex
+        }));
+
+        var grouped = allRecords
+            .GroupBy(r => r.RowIndex)
+            .Select(group => group.ToDictionary(g => g.ColumnName, g => g.Value))
+            .ToList();
+
+        return grouped;
+    }
+
     public async Task<ErrorOr<int>> CreateColumn(CreateColumnDto model, CancellationToken ct)
     {
         var existsUser = await unitOfWork.TablesRepository.ExistsAsync(model.TableId, ct);
         if (!existsUser)
             return ErrorOr.Error.NotFound(description: DatabaseMessage.NotFound);
-       
+
         var Column = new Columns()
         {  //maping
             TablesId = model.TableId,
             ColumnType = model.ColumnType,
             ColumnName = model.ColumnName,
-            FakeDataTypes =(FakeDataTypes)(Enum.IsDefined(model.FakeDataTypes) ? model.FakeDataTypes : FakeDataTypesDto.None)
-                
+            FakeDataTypes = (FakeDataTypes)(Enum.IsDefined(model.FakeDataTypes) ? model.FakeDataTypes : FakeDataTypesDto.None)
+
         };
 
         await unitOfWork.ColumnsRepository.AddAsync(Column, ct);
@@ -46,7 +68,7 @@ public class ColumnService(IUnitOfWork unitOfWork) : IColumnService
         findColumn.ColumnType = model.ColumnType;
         findColumn.ColumnName = model.ColumnName;
         findColumn.FakeDataTypes =
-            (FakeDataTypes)(Enum.IsDefined(model.FakeDataTypes) 
+            (FakeDataTypes)(Enum.IsDefined(model.FakeDataTypes)
                 ? model.FakeDataTypes : FakeDataTypesDto.None);
         await unitOfWork.ColumnsRepository.UpdateAsync(findColumn, ct);
         await unitOfWork.SaveChangesAsync(ct);
@@ -69,7 +91,7 @@ public class ColumnService(IUnitOfWork unitOfWork) : IColumnService
         if (findColumn is null)
             return ErrorOr.Error.NotFound(description: ColumnMessage.NotFound);
 
-        return new ColumnItemDto(findColumn.Id, findColumn.ColumnName, findColumn.ColumnType, (FakeDataTypesDto)(findColumn.FakeDataTypes), findColumn.TablesId,null);
+        return new ColumnItemDto(findColumn.Id, findColumn.ColumnName, findColumn.ColumnType, (FakeDataTypesDto)(findColumn.FakeDataTypes), findColumn.TablesId, null);
     }
 
     public async Task<ErrorOr<IEnumerable<ColumnItemDto>>> GetColumnsByTableId(int tableId, CancellationToken ct)
@@ -78,6 +100,6 @@ public class ColumnService(IUnitOfWork unitOfWork) : IColumnService
         if (findColumn is null)
             return ErrorOr.Error.NotFound(description: ColumnMessage.NotFound);
 
-        return findColumn.Select(_ => new ColumnItemDto(_.Id, _.ColumnName, _.ColumnType, (FakeDataTypesDto)(_.FakeDataTypes), _.TablesId,null)).ToList();
+        return findColumn.Select(_ => new ColumnItemDto(_.Id, _.ColumnName, _.ColumnType, (FakeDataTypesDto)(_.FakeDataTypes), _.TablesId, null)).ToList();
     }
 }
--- MockPars.Application/Services/Interfaces/IColumnService.cs
+++ MockPars.Application/Services/Interfaces/IColumnService.cs
@@ -5,6 +5,7 @@ namespace MockPars.Application.Services.Interfaces;
 
 public interface IColumnService
 {
+    Task<List<Dictionary<string, string>>> GetAllRowDataAsync(int tableId, CancellationToken ct);
     Task<ErrorOr<int>> CreateColumn(CreateColumnDto model, CancellationToken ct);
     Task<ErrorOr<int>> UpdateColumn(UpdateColumnDto model, CancellationToken ct);
     Task<ErrorOr<bool>> DeleteColumn(int id, CancellationToken ct);
