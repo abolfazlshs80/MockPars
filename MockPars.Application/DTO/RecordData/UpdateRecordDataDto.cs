4ee3fd85bfa21b3c146778780fdc169582758131
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-26 16:29:59 +0330

    add RowIndex in recordData

--- MockPars.Application/DTO/RecordData/UpdateRecordDataDto.cs
+++ MockPars.Application/DTO/RecordData/UpdateRecordDataDto.cs
@@ -5,7 +5,7 @@ namespace MockPars.Application.DTO.RecordData;
 public class UpdateRecordDataDto
 {
     public int Id { get; set; }
-
+    public int RowIndex { get; set; }
     public string Value { get; set; }
     public int ColumnsId { get; set; }
 }
