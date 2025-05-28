bf52c96674f168f3c6c2a0c6b8e593ec37b9d6c2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-28 08:31:01 +0330

    add login dto

---
+++ MockPars.Infrastructure/Models/Authebntication/UserLoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockPars.Infrastructure.Models.Authebntication
+{
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class UserRegisterDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
