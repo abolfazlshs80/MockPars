4bdd1b0fcdc77405e0a4570d4896b3d9a480eae5
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-28 21:55:43 +0330

    add ConnectToDatabase method

--- MockPars.WebApi/Controllers/DatabaseController.cs
+++ MockPars.WebApi/Controllers/DatabaseController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using MockPars.Application.DTO.Database;
+using MockPars.Application.DTO.Table;
 using MockPars.Application.DTO.Users;
 using MockPars.Application.Extention;
 using MockPars.Application.Services.Interfaces;
@@ -10,7 +12,7 @@ namespace MockPars.WebApi.Controllers
     [Authorize]
     [ApiController]
     [Route("api/[controller]")]
-    public class DatabaseController(IDatabaseService databaseService) : ControllerBase
+    public class DatabaseController(IDatabaseService databaseService,ISqlProvider sqlProvider) : ControllerBase
     {
         [HttpPost]
         public async Task<IActionResult> Create(CreateDatabaseDto model, CancellationToken ct)
@@ -71,6 +73,31 @@ namespace MockPars.WebApi.Controllers
                 return BadRequest(string.Join(",", result.Errors.Select(a => a.Description)));
 
             return Ok(result.Value);
+        }
+
+
+        [HttpPost("ConnectToDatabase")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConnectToDatabase(ConnectionDatabaseDto model,CancellationToken ct)
+        {
+            return Ok(await sqlProvider.GetTablesAsync(model,ct));
+        }
+
+
+        [HttpPost("FakeDataToTable")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConnectToDatabase(FakeDataToTableDto model, CancellationToken ct)
+        {
+            var res = await sqlProvider.AddFakeDataAsync(model);
+            return Ok();
+        }
+
+        [HttpPost("GetColumnsByTable")]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConnectToDatabase(GetColumnByTableDto model, CancellationToken ct)
+        {
+            var res = await sqlProvider.GetTableColumnAsync(model);
+            return Ok(res);
         }
     }
 }
