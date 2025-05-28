3be76563e730d92adb37c93e4f7973c2c791b894
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-28 08:29:47 +0330

    add jwt

---
+++ MockPars.Infrastructure/Service/Jwt/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using MockPars.Domain.Models;
+using System.Security.Claims;
+
+namespace MockPars.Infrastructure.Service.Jwt;
+
+public interface IJwtService
+{
+    public string GenerateJwtToken(User user);
+
+    Task<ClaimsPrincipal?> ValidateAndExtractClaimsAsync(string token);
+}
--- MockPars.Infrastructure/Service/Jwt/Jwtservices.cs
+++ MockPars.Infrastructure/Service/Jwt/Jwtservices.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using MockPars.Domain.Models;
 using MockPars.Infrastructure.Models.Jwt;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using static SampleWebApi.Repository.Models.comment.Jwtservices;
 
-namespace SampleWebApi.Repository.Models.comment
+namespace MockPars.Infrastructure.Service.Jwt
 {
-    public class Jwtservices( IOptions<ConfigJwtDto> jwtConfigureOptions) : IJwtService
+    public class JwtService( IOptions<ConfigJwtDto> jwtConfigureOptions) : IJwtService
     {
         public string GenerateJwtToken(User user)
         {
@@ -19,15 +18,9 @@ namespace SampleWebApi.Repository.Models.comment
             var claims = new[]
             {
                 new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
         
             };
-
-            var token = new JwtSecurityToken(
-                issuer: jwtConfigureOptions.Value.Issuer,
-                audience: jwtConfigureOptions.Value.Audience,
-                claims: claims,
-                expires: DateTime.UtcNow.AddHours(1),
-                signingCredentials: credentials);
             var symmetricSecurityKey = securityKey;
             var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
 
@@ -37,14 +30,42 @@ namespace SampleWebApi.Repository.Models.comment
                 claims: claims,
                 expires: DateTime.UtcNow.AddMinutes(60),
                 signingCredentials: signingCredentials);
+         
             return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
 
 
         }
 
-        public interface IJwtService
+        public async Task< ClaimsPrincipal?> ValidateAndExtractClaimsAsync(string token)
         {
-            public string GenerateJwtToken(User user);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(jwtConfigureOptions.Value.Key);
+
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidIssuer = jwtConfigureOptions.Value.Issuer,
+
+                ValidateAudience = false,
+                ValidAudience = jwtConfigureOptions.Value.Audience,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero // برای دقت در اعتبار سنجی تاریخ انقضا
+            };
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+                return principal;
+            }
+            catch (Exception ex)
+            {
+                // توکن نامعتبر است
+                return null;
+            }
         }
     }
 }
