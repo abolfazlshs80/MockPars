c55973bbf4a23cbd29d655f70fdfbb170293bde2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-29 12:25:27 +0330

    Add foreign key properties to ColumnInfoDto
    Updated `ColumnInfoDto` class to include `IsForeignKey` and `TableForeignKeyName` properties. Modified `using` directives to include `System.ComponentModel.DataAnnotations.Schema`.

--- MockPars.Application/DTO/Database/ColumnInfoDto.cs
+++ MockPars.Application/DTO/Database/ColumnInfoDto.cs
@@ -1,14 +1,18 @@
-﻿using MockPars.Application.DTO.@base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using MockPars.Application.DTO.@base;
 
 namespace MockPars.Application.DTO.Database;
 
 public class ColumnInfoDto
 {
+    
     public string Name { get; set; }
     public string TypeColumn { get; set; }
     public bool IsPrimaryKey { get; set; }
     public bool IsComputed { get; set; }
     public bool IsNullable { get; set; }
+    public bool IsForeignKey { get; set; }
+    public string TableForeignKeyName { get; set; }
 
 
 }
