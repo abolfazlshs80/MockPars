90d182a6cc1169aad1d2182b2617d35ed6f914ae
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-29 08:23:41 +0330

    Add DTO classes for database column and connection info
    Introduced `ColumnInfoDto` for column metadata with properties
    like `Name`, `TypeColumn`, `IsPrimaryKey`, `IsComputed`,
    and `IsNullable`. Added `ConnectionDatabaseDto` for
    storing the database connection string.

---
+++ MockPars.Application/DTO/Database/ColumnInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using MockPars.Application.DTO.@base;
+
+namespace MockPars.Application.DTO.Database;
+
+public class ColumnInfoDto
+{
+    public string Name { get; set; }
+    public string TypeColumn { get; set; }
+    public bool IsPrimaryKey { get; set; }
+    public bool IsComputed { get; set; }
+    public bool IsNullable { get; set; }
+
+
+}
---
+++ MockPars.Application/DTO/Database/ConnectionDatabaseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockPars.Application.DTO.Database
+{
+    public class ConnectionDatabaseDto
+    {
+        public string ConnectionString { get; set; }
+    }
+}
