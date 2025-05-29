ab16ad988d2e098b41324a177550f85c7df7ef7f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-29 08:23:59 +0330

    Add DTO classes for database and table information
    Introduces `FakeDataToTableDto` and `GetColumnByTableDto`
    in `FakeDataToTableDto.cs` for handling database connections
    and table data. Also adds `TableInfoDto` in `TableInfoDto.cs`
    to represent table names and schemas.

---
+++ MockPars.Application/DTO/Table/FakeDataToTableDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MockPars.Application.DTO.Database;
+
+namespace MockPars.Application.DTO.Table
+{
+    public class FakeDataToTableDto
+    {
+        public ConnectionDatabaseDto Database { get; set; }
+        public List<TableInfoDto> Tables { get; set; }
+        public int Count { get; set; }
+    }
+    public class GetColumnByTableDto
+    {
+        public ConnectionDatabaseDto Database { get; set; }
+      public  TableInfoDto Table { get; set; }
+   
+    }
+}
---
+++ MockPars.Application/DTO/Table/TableInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockPars.Application.DTO.Table
+{
+public class TableInfoDto
+    {
+        public string TableName { get; set; }
+        public string Schema { get; set; }
+    }
+}
