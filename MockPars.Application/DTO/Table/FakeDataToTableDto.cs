28fcfd557ac2f7d450597517f02293bc654fa34a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-30 08:59:32 +0330

    Add DTO classes for custom columns and fake data types
    Introduces `FakeDataToCustomColumnsDto` and `FakeDataToColumnDto` classes in the `MockPars.Application.DTO.Table` namespace. The new classes include properties for database connection, table information, and fake data types. Also adds a using directive for `MockPars.Application.DTO.@base`.

--- MockPars.Application/DTO/Table/FakeDataToTableDto.cs
+++ MockPars.Application/DTO/Table/FakeDataToTableDto.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MockPars.Application.DTO.@base;
 using MockPars.Application.DTO.Database;
 
 namespace MockPars.Application.DTO.Table
 {
+
+    public class FakeDataToCustomColumnsDto
+    {
+        public ConnectionDatabaseDto Database { get; set; }
+        public TableInfoDto Tables { get; set; }
+        public List<FakeDataToColumnDto> Columns { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class FakeDataToColumnDto
+    {
+        public string Name { get; set; }
+        public FakeDataTypesDto FakeDataTypesDto { get; set; }
+    }
     public class FakeDataToTableDto
     {
         public ConnectionDatabaseDto Database { get; set; }
