0512a52eb7866e3b758231bc42bd1836d4c1273d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-02 15:31:15 +0330

    Add ColumnValues class to ColumnItemDto.cs
    Introduced a new class `ColumnValues` with properties `Name` and `Value` to represent column values. The existing `ColumnItemDto` record remains unchanged.

--- MockPars.Application/DTO/Column/ColumnItemDto.cs
+++ MockPars.Application/DTO/Column/ColumnItemDto.cs
@@ -4,3 +4,9 @@ using MockPars.Application.DTO.RecordData;
 namespace MockPars.Application.DTO.Column;
 
 public record ColumnItemDto(int Id,string ColumnName, string ColumnType, FakeDataTypesDto FakeDataTypes,int TableId, IEnumerable<RecordDataItemDto> RecordData);
+
+public class ColumnValues
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
