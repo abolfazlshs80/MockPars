e90fcbb455c7c3e4307a212e5f76b880fba3393d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-06-02 15:31:45 +0330

    Add GetByColumnNameAsync method to IColumnsRepository
    Introduced a new asynchronous method `GetByColumnNameAsync`
    in the `IColumnsRepository` interface. This method accepts
    a string parameter `name` and a `CancellationToken`,
    returning a `Task<Columns>` to retrieve a `Columns` object
    based on the specified column name.

--- MockPars.Domain/Interface/IColumnsRepository.cs
+++ MockPars.Domain/Interface/IColumnsRepository.cs
@@ -4,6 +4,7 @@ namespace MockPars.Domain.Interface;
 
 public interface IColumnsRepository : IRepository<Columns>
 {
+    Task<Columns> GetByColumnNameAsync(string name, CancellationToken ct);
     Task<Columns> GetByIdAsync(int id, int tableId,CancellationToken ct);
     Task<IEnumerable<Columns>> GetByTableIdAsync( int tableId,CancellationToken ct);
     Task<List<Columns>> GetAllRowDataAsync(int tableId, CancellationToken ct);
